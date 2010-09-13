61e6843ffc852a29d29379ac433f6bdedc02f893
Antoine Hersen <antoine.hersen@gmail.com>
2010-09-12 22:11:56 -0500

    first version of flood bot

--- cpp_starter_package/MyBot.cc
+++ cpp_starter_package/MyBot.cc
@@ -3,7 +3,7 @@
 
 int  ClosestPlanet(const PlanetWars& pw,
                    int planet_source_id) {
-  int result = 0;
+  int result = -1;
   int shortest_distance = 99999;
   std::vector<Planet> not_my_planets = pw.NotMyPlanets();
   for (int i = 0; i < not_my_planets.size(); ++i) {
@@ -18,16 +18,45 @@ int  ClosestPlanet(const PlanetWars& pw,
   return result;
 }
 
+int  EasiestPlanet(const PlanetWars& pw,
+                   int planet_source_id) {
+  int result = -1;
+  int shortest_distance = 99999;
+
+  int own_gr = pw.GetPlanet(planet_source_id).GrowthRate();
+
+  std::vector<Planet> not_my_planets = pw.NotMyPlanets();
+  for (int i = 0; i < not_my_planets.size(); ++i) {
+    const Planet& p = not_my_planets[i];
+
+    int distance = pw.Distance( p.PlanetID(), planet_source_id);
+    distance += p.NumShips() / own_gr;
+
+    if (distance < shortest_distance) {
+      shortest_distance = distance;
+      result = p.PlanetID();
+    }
+  }
+  return result;
+}
+
+int Attacked(const PlanetWars& pw,
+             int planet_source_id) {
+
+  int result = 0;
+  std::vector<Fleet> enemy_fleets = pw.EnemyFleets();
+  for (int i = 0; i < enemy_fleets.size(); ++i) {
+    const Fleet & fleet = enemy_fleets[i];
+    if (fleet.DestinationPlanet() == planet_source_id) {
+      result += fleet.NumShips();
+    }
+  }
+  return result;
+}
+
 // The DoTurn function is where your code goes. The PlanetWars object contains
-// the state of the game, including information about all planets and fleets
-// that currently exist. Inside this function, you issue orders using the
-// pw.IssueOrder() function. For example, to send 10 ships from planet 3 to
-// planet 8, you would say pw.IssueOrder(3, 8, 10).
-//
-// There is already a basic strategy in place here. You can use it as a
-// starting point, or you can throw it out entirely and replace it with your
-// own. Check out the tutorials and articles on the contest website at
-// http://www.ai-contest.com/resources.
+
+
 void DoTurn(const PlanetWars& pw) {
 
   std::vector<Planet> my_planets = pw.MyPlanets();
@@ -35,9 +64,11 @@ void DoTurn(const PlanetWars& pw) {
     const Planet& p = my_planets[i];
     int source = p.PlanetID();
     int dest = ClosestPlanet( pw, source);
-
-    pw.IssueOrder( source, dest, p.NumShips());
-
+    if (dest != -1) {
+      int num_ships = p.NumShips() - Attacked(pw, source);
+      if (num_ships > 0)
+        pw.IssueOrder( source, dest, num_ships);
+    }
   }
 }
 
